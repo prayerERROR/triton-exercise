dd6a3d5be2801fa8031fc47bec32d202a4b13ba0
prayerERROR <qcsdbzzt3@gmail.com>
2025-07-30 15:46:03 -0700

    add benchmark for vector operator

--- vector_op.py
+++ vector_op.py
@@ -5,6 +5,9 @@ import torch
 import triton
 import triton.language as tl
 
+# Config device
+DEVICE = "cuda"
+
 # Add kernel
 @triton.jit
 def add_kernel(a_ptr, b_ptr, out_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
@@ -25,6 +28,21 @@ def add(a, b):
     grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
     add_kernel[grid](a, b, out, n_elements, BLOCK_SIZE=256)
     return out
+
+def benchmark_add():
+    sizes = [2**n for n in range(10, 25, 1)]
+    quantiles = [0.5, 0.2, 0.8]
+    torch_perf, triton_perf = [], []
+    for size in sizes:
+        torch.cuda.empty_cache()
+        x = torch.rand(size, device=DEVICE)
+        y = torch.rand(size, device=DEVICE)
+        GBps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+        ms, ms_max, ms_min = triton.testing.do_bench(lambda: x+y, quantiles=quantiles)
+        torch_perf.append((GBps(ms), GBps(ms_max), GBps(ms_min)))
+        ms, ms_max, ms_min = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
+        triton_perf.append((GBps(ms), GBps(ms_max), GBps(ms_min)))
+    return torch_perf, triton_perf
 
 
 # Mul kernel
@@ -47,6 +65,21 @@ def mul(a, b):
     mul_kernel[grid](a, b, out, n_elements, BLOCK_SIZE=256)
     return out
 
+def benchmark_mul():
+    sizes = [2**n for n in range(10, 25, 1)]
+    quantiles = [0.5, 0.2, 0.8]
+    torch_perf, triton_perf = [], []
+    for size in sizes:
+        torch.cuda.empty_cache()
+        x = torch.rand(size, device=DEVICE)
+        y = torch.rand(size, device=DEVICE)
+        GBps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+        ms, ms_max, ms_min = triton.testing.do_bench(lambda: x*y, quantiles=quantiles)
+        torch_perf.append((GBps(ms), GBps(ms_max), GBps(ms_min)))
+        ms, ms_max, ms_min = triton.testing.do_bench(lambda: mul(x,y), quantiles=quantiles)
+        triton_perf.append((GBps(ms), GBps(ms_max), GBps(ms_min)))
+    return torch_perf, triton_perf
+
 
 # Exp kernel
 @triton.jit
@@ -64,3 +97,17 @@ def exp(a):
     grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
     exp_kernel[grid](a, out, n_elements, BLOCK_SIZE=256)
     return out
+
+def benchmark_exp():
+    sizes = [2**n for n in range(10, 25, 1)]
+    quantiles = [0.5, 0.2, 0.8]
+    torch_perf, triton_perf = [], []
+    for size in sizes:
+        torch.cuda.empty_cache()
+        x = torch.rand(size, device=DEVICE)
+        GBps = lambda ms: 2 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3)
+        ms, ms_max, ms_min = triton.testing.do_bench(lambda: torch.exp(x), quantiles=quantiles)
+        torch_perf.append((GBps(ms), GBps(ms_max), GBps(ms_min)))
+        ms, ms_max, ms_min = triton.testing.do_bench(lambda: exp(x), quantiles=quantiles)
+        triton_perf.append((GBps(ms), GBps(ms_max), GBps(ms_min)))
+    return torch_perf, triton_perf
